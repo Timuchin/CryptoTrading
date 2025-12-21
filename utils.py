b79aeeb857707293b2d8a872f555c1facad44494
Timur Zekokh <timurzekokh@gmail.com>
2025-12-22 02:17:14 +0300

    fixed utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def getdata(client, symbol:str, interval:str, start_time:str = None, end_time:str = None) -> pd.DataFrame:
     historical_data = pd.DataFrame(client.get_historical_klines(symbol, interval, start_time, end_time))
-    historical_data = historical_data.iloc[:, :6] # Время открытия свечи, цена открытия, закрытия, объем
+    historical_data = historical_data.iloc[:, :6] # datetime\date, OHLC and volume in tokens (not USD)
     historical_data.columns = ['date', 'open_price', 'high_price', 'low_price', 'close_price', 'volume']
     historical_data = historical_data.set_index('date')
     historical_data.index = pd.to_datetime(historical_data.index, unit='ms')
